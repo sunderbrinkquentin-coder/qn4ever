76678fdba69da000fde69521c6815733baa96fc8
sunderbrinkquentin-coder <sunderbrinkquentin@gmail.com>
2026-05-14 10:12:37 +0200

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+# Im VS-Terminal oder PowerShell:
+git rm -r --cached .vs bin obj
+git commit -m "Remove cached build directories"
